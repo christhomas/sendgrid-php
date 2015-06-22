e368f76252deade9291fee12cec1a161d97b0419
Chris Thomas <chris.thomas@antimatter-studios.com>
2015-06-22 08:56:27 +0200

    add the functionality to create a template for sendgrid

--- lib/SendGrid/Template.php
+++ lib/SendGrid/Template.php
@@ -6,8 +6,10 @@ class Template
 	protected $sendgrid;
 
 	protected $url = array(
-		"list"	=> "https://api.sendgrid.com/v3/templates",
-		"get"	=> "https://api.sendgrid.com/v3/templates/(:template_id)"
+		"list"		=> "https://api.sendgrid.com/v3/templates",
+		"get"		=> "https://api.sendgrid.com/v3/templates/(:template_id)",
+		"create"	=> "https://api.sendgrid.com/v3/templates",
+		"edit"		=> "https://api.sendgrid.com/v3/templates/(:template_id)/versions"
 	);
 
     public function __construct($sendgrid)
@@ -35,5 +37,40 @@ class Template
     	$template = $this->sendgrid->getRequest($url);
 
     	return $template->body;
+    }
+
+    public function create($name)
+    {
+    	$data = array("name"=>$name);
+
+    	$template = $this->sendgrid->postRequest($this->url["create"],json_encode($data));
+
+    	$template = $template->body;
+
+    	return $template["id"];
+    }
+
+    public function editById($id_template,$name,$subject,$active,$html)
+    {
+		$data = array(
+			"name"			=> $name,
+			"subject"		=> "<%subject%>$subject",
+			"active"		=> intval($active),
+			"html_content"	=> $html,
+			"plain_content"	=> "<%body%>"
+		);
+
+		$url = $this->url["edit"];
+		$url = str_replace("(:template_id)",$id_template,$url);
+
+		$template = $this->sendgrid->postRequest($url,json_encode($data));
+		$template = $template->body;
+
+		return $template["template_id"];
+    }
+
+    public function delete($id_template)
+    {
+		//	TODO: add code that deletes a template
     }
 }
